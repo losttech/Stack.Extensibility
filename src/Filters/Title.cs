865261bf016421b09cc133ac2d5a9d2742979c19
Victor Nova <lost@losttech.software>
2022-07-27 13:47:16 -0700

    do not raise and swallow exception when window disappears

--- src/Filters/Title.cs
+++ src/Filters/Title.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.Diagnostics;
 
 using PInvoke;
@@ -10,11 +11,48 @@ public sealed class Title : CommonStringMatchFilter, IFilter<IntPtr> {
         if (this.MatchesAnything()) return true;
 
         try {
-            string title = User32.GetWindowText(windowHandle);
+            string? title = GetWindowText(windowHandle);
+            if (title is null) return false;
             return this.Matches(title);
         } catch (Win32Exception e) {
             Debug.WriteLine($"Can't obtain window title: {e}");
             return false;
+        }
+    }
+
+    static string? GetWindowText(IntPtr hwnd) {
+        while (true) {
+            int maxLen = User32.GetWindowTextLength(hwnd);
+            if (maxLen == 0) {
+                var error = Kernel32.GetLastError();
+                return error switch {
+                    Win32ErrorCode.ERROR_SUCCESS => string.Empty,
+                    Win32ErrorCode.ERROR_INVALID_WINDOW_HANDLE => null,
+                    _ => throw new Win32Exception(error),
+                };
+            }
+
+
+            char[] buffer = ArrayPool<char>.Shared.Rent(maxLen + 2);
+            try {
+                int finalLength = User32.GetWindowText(hwnd, buffer, maxLen + 1);
+                
+                if (finalLength == 0) {
+                    var error = Kernel32.GetLastError();
+                    return error switch {
+                        Win32ErrorCode.ERROR_SUCCESS => string.Empty,
+                        Win32ErrorCode.ERROR_INVALID_WINDOW_HANDLE => null,
+                        _ => throw new Win32Exception(error),
+                    };
+                }
+                
+                if (finalLength > maxLen)
+                    continue;
+
+                return new string(buffer, 0, finalLength);
+            } finally {
+                ArrayPool<char>.Shared.Return(buffer);
+            }
         }
     }
 }
--- src/Filters/Win32Class.cs
+++ src/Filters/Win32Class.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Buffers;
 using System.Diagnostics;
 
 using PInvoke;
+
+using static PInvoke.Win32ErrorCode;
 
 namespace LostTech.Stack.Extensibility.Filters;
 
@@ -10,11 +13,40 @@ public sealed class Win32Class : CommonStringMatchFilter, IFilter<IntPtr> {
         if (this.MatchesAnything()) return true;
 
         try {
-            string className = User32.GetClassName(windowHandle, maxLength: 4096);
+            string? className = GetClassName(windowHandle);
+            if (className is null) return false;
             return this.Matches(className);
         } catch (Win32Exception e) {
             Debug.WriteLine($"Can't obtain window class: {e}");
             return false;
+        }
+    }
+
+    static string? GetClassName(IntPtr hwnd) {
+        int maxLen = 256;
+        while (true) {
+            char[] buffer = ArrayPool<char>.Shared.Rent(maxLen + 2);
+            try {
+                int finalLength = User32.GetClassName(hwnd, buffer, maxLen + 1);
+
+                if (finalLength == 0) {
+                    var error = Kernel32.GetLastError();
+                    return error switch {
+                        ERROR_SUCCESS => string.Empty,
+                        ERROR_INVALID_WINDOW_HANDLE => null,
+                        _ => throw new PInvoke.Win32Exception(error),
+                    };
+                }
+
+                if (finalLength > maxLen) {
+                    maxLen = checked(maxLen * 2);
+                    continue;
+                }
+
+                return new string(buffer, 0, finalLength);
+            } finally {
+                ArrayPool<char>.Shared.Return(buffer);
+            }
         }
     }
 }
