b028635fc3f62b4df92d4a642fb5cfc35b53130a
Victor Nova <lost@losttech.software>
2025-06-12 12:48:12 -0700

    added FilterWrapper

---
+++ src/Filters/FilterWrapper.cs
@@ -0,0 +1,6 @@
+﻿namespace LostTech.Stack.Extensibility.Filters;
+
+public sealed class FilterWrapper: IFilter<IntPtr> {
+    public bool Matches(nint value) => this.Filter?.Matches(value) == true;
+    public IFilter<IntPtr>? Filter { get; set; }
+}
---
+++ src/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using static System.FormattableString;
